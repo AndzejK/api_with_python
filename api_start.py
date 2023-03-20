f9d4a367a1a52d21aa0d1797339969870f2efd7d
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-03-20 11:55:55 +0200

    get data from API and save to csv file

--- api_start.py
+++ api_start.py
@@ -1,1 +1,17 @@
-# My API Key -  29d5133045294bd9b81a0c49a14b32f2 from https://newsapi.org
+import requests # for HTTP
+import os
+my_API_key=os.getenv('personal_api_key') 
+API_from_tutorial="890603a55bfa47048e4490069ebee18c" 
+
+api_link=f"https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2023-3-18&to=2023-3-20&sortBy=popularity&language=en&apiKey={my_API_key}"
+
+http_req=requests.get(api_link)
+stored_content_in_dictionary_form=http_req.json() #JSON in dictionary
+articles=stored_content_in_dictionary_form["articles"]
+#print(articles[1]["title"])
+recent_articles=''
+for article in articles:
+    recent_articles+=(f"\nTitle: {article['title']},\n\tDescription:\n\t{article['description']}")
+    print(recent_articles)
+    with open("artciles.csv", 'w') as csv_file:
+        csv_file.write(recent_articles)
