0c2960abdff4fc615b13fb20f8e6746be25bc045
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-03-21 08:48:21 +0200

    fetching info from API, combing data, appending the data to an external file

--- get_weather_forecast.py
+++ get_weather_forecast.py
@@ -1,4 +1,6 @@
 #openweathermap.org 
+"""Search weather forecast for 5 days with data every 3 hours by city name """
+
 import requests
 import os
 
@@ -9,6 +11,7 @@ request_by_city=f"https://api.openweathermap.org/data/2.5/forecast?q={city_name}
 api=requests.get(request_by_city)
 data=api.json() #JSON in dictionary
 org_list_from_openweathermap=data["list"]
+msc=data["city"] #getting info about city
 
 #storing conditions info in the list, e.x. 'clear sky', 'light rain', 'light rain'
 conditions=[]
@@ -16,4 +19,28 @@ for all in org_list_from_openweathermap:
     for desc in ((all["weather"])):
         conditions.append(desc["description"])
 
-print(conditions)
+#storing date and time         
+time_dt=[]
+for time in org_list_from_openweathermap:
+    time_dt.append(time["dt_txt"])
+
+#Storing temprature 
+tempratures=[]
+for temp in org_list_from_openweathermap:
+    tempratures.append(temp["main"]["temp"])
+
+#Fetching city name from API
+city=msc["name"]
+
+# Combing all data and write/add to  the file
+def combines_data(time,temp,cond):
+    city=city_name
+    line=''
+    i=0
+    while i<len(time):
+        line+=(f"{city}, {time[i]}, {temp[i]}, {cond[i]}\n")
+        i+=1
+    # Mistake was having the open() method inside the loop and caused some unnecessary repetition   
+    with open("data.txt","a") as file: # append to the file 
+        file.write(line)
+combines_data(time_dt,tempratures,conditions)
